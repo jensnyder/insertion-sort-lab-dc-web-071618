2825617201070ed2adadf156ea9bc5526a4ee081
jensnyder <jlsnyder09@gmail.com>
2018-11-02 11:38:20 -0400

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-function findMinAndRemove(array){
-
+function findMinAndRemove(array) {
+  let min = array[0];
+  let minIndex = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < min) {
+      min = array[i];
+      minIndex = i;
+    }
+  }
+  array.splice(minIndex, 1);
+  return min;
 }
 
-function insertionSort(array){
-
+function insertionSort(array) {
+  let sortedArray = [];
+  while (array.length > 0) {
+    let minimum = findMinAndRemove(array);
+    sortedArray.push(minimum);
+  }
+  return sortedArray;
 }
